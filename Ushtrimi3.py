fce25a1369c2391cf638a92eac30c66380c5b572
vdauti97 <39677424+vdauti97@users.noreply.github.com>
2022-01-26 14:45:58 +0100

    Update Ushtrimi3.py

--- Ushtrimi3.py
+++ Ushtrimi3.py
@@ -1,25 +1,25 @@
-'''
+"""
 Ushtrimi3:
 - gjeneroni nje numer random integer nga 0-10
-- Permes nje for loop me rang 1-10 dhe permes while loop ju duhet te merrni inputin e userit dhe ta krahasoni ate me numrin e gjeneruar random.
+- Permes nje for loop me range 1-10 dhe permes while loop ju duhet te merrni inputin e userit dhe ta krahasoni ate me numrin e gjeneruar random.
 - Mund te perdorni cfaredo funksioni ose variable ndihmese qe do e beje me te lehte kalkulimin.
 - Ne rast se nuk eshte ofruar asnje input. Pra input==‘’,  njoftojeni userin me mesazh perkates
 - Njoftojeni userin nese ka dhene input te gabuar ose te sakte.
 - Ne rastin e while loop mos perdorni break statements per te dale nga unaza.
-'''
+"""
 
 import random
 
-numri_random = random.randint(1,10)
+numri_random = random.randint(1, 10)
 numri_random_str = str(numri_random)
 # print(numri_random)
 
 for i in range(10):
-    inputi_i_userit = input('Shkruaj nje numer:')
-    if inputi_i_userit == '':
-        print('Ju lutem shkruani dicka')
+    inputi_i_userit = input("Shkruaj nje numer:")
+    if inputi_i_userit == "":
+        print("Ju lutem shkruani dicka")
     elif inputi_i_userit != numri_random_str:
-        print('e pasakte')
+        print("e pasakte")
     else:
-        print('bravo')
+        print("bravo")
         break
